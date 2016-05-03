afc6957fac14412a97ee4aef677d12aa4ce2e041
egorsh0 <egorsh@inbox.ru>
2016-05-03 23:51:34 +0300

    Временная корректировка отображения

--- script/script.js
+++ script/script.js
@@ -1,12 +1,13 @@
 // Начинаем работу когда страница полностью загружена (включая графику)
-$(window).load(
-  function svgElementClicked(theElement)
-		{
-			var s = document.getElementById("status");
-			if(theElement.id != 'lmap')
-			{
-				s.textContent = "Корпус " + theElement.id;
-			}else
-			s.textContent = " ";
-		}
-);
+$(window).load(function () {
+  // Получаем доступ к SVG DOM
+  var svgobject = document.getElementById('imap'); 
+  if ('contentDocument' in svgobject)
+    var svgdom = svgobject.contentDocument;
+
+  // Хак для WebKit (чтобы правильно масштабировал нашу карту)
+  var viewBox = svgdom.rootElement.getAttribute("viewBox").split(" ");
+  var aspectRatio = viewBox[2] / viewBox[3];
+  svgobject.height = parseInt(svgobject.offsetWidth / aspectRatio);
+  });
+  
